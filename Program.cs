72b9355c14fe6416dd63bbe119fd2de7f6267904
Fernaniii <fernandojosem931@gmail.com>
2024-06-12 18:01:20 -0800

    console app 1

---
+++ Program.cs
@@ -0,0 +1,7 @@
+﻿string firstName = ("Fernando Jose");
+string middleName = ("Maniaga");
+string lastName  = ("Magnaye");
+int myAge = 22;
+
+Console.WriteLine("my Full name is " + " " +firstName +" "+ middleName +" "+ lastName );
+Console.WriteLine("my age is " + myAge);
