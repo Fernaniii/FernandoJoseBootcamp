dddbbb5940336d8876fa1991a5c9708bbdd4fc74
Fernaniii <fernandojosem931@gmail.com>
2024-06-23 07:14:47 -0800

    be consistent

---
+++ DAY1/Program.cs
@@ -0,0 +1,59 @@
+﻿
+//DAY 1.1
+/**
+string firstName = ("Fernando Jose");
+string middleName = ("Maniaga");
+string lastName  = ("Magnaye");
+int myAge = 22;
+string myGender = "MALE";
+
+Console.WriteLine($"my Full name is {firstName} {middleName} {lastName}" );
+Console.WriteLine($"my age is  {myAge} and I am a {myGender}");
+**/
+
+
+//DAY 1.2
+/**
+Random random = new Random();
+int diceOne = random.Next(7);
+int diceTwo = random.Next(7);
+int diceThree = random.Next(7);
+int bonus = 0;
+
+if(diceOne == diceTwo && diceTwo == diceThree){
+    bonus = 6;
+}
+
+int  Score = diceOne + diceTwo + diceThree + bonus;
+Console.WriteLine($"Dice Roll: {diceOne} + {diceTwo} + {diceThree} + BONUS({bonus}) = {Score}");
+if(Score >= 15){
+    Console.WriteLine(" CONGRATS!! YOU WON");
+} else{
+    Console.WriteLine(" SORRY.. YOU LOST ");
+}
+**/
+
+// DAY 3
+/**
+//interpolation $(" write {nameOfVariable}")
+int num1 = 95;
+8int num2 = 80;
+int num3 = 90;
+
+Console.WriteLine($"numbers [{num1}, {num2}, {num3}]. ");
+
+int highest = (num1 > num2) ? (num1 > num3 ? num1 : num3) : (num2 > num3 ? num2 : num3);
+int lowest = (num1 < num2) ? (num1 < num3 ? num1 : num3) : (num2 < num3 ? num2 : num3);
+
+Console.WriteLine("HIGHEST: " + highest);
+Console.WriteLine("LOWEST: " + lowest);
+**/
+
+//DAY 4
+int numberToCheck = 9;
+ 
+if(numberToCheck %2 == 0){
+    Console.WriteLine($"Number {numberToCheck} is even");
+} else{
+    Console.WriteLine($"Number {numberToCheck} is not even");
+}
